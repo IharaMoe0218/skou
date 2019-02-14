3bb68eae05fd2e22d78df7506692509e0d20ce4a
Ihara Moe <1>
2019-02-14 13:39:13 +0900

    yunitliwomawasia

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -26,4 +26,14 @@ bool Game::Start()
 
 void Game::Update()
 {
+	//YŽ²Žü‚è‚É
+	CQuaternion qAddRot;
+	qAddRot.SetRotationDeg(
+		{ 0.0f,1.0f,0.0f },
+		5.0f
+	);
+
+	//‰ñ“]
+	m_rotation *= qAddRot;
+	m_skinModelRender->SetRotation(m_rotation);
 }
